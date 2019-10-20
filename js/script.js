4e4aa3b6ea86450218c72fa6960169769031a118
Jessiane Leal <jessiane.leal@hotmail.com>
2019-10-20 11:39:09 -0300

    Desenvolvimento de Landing Page de cadastro ao Sea-Fi

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$('.nav__sf a').click(function() {
+    if ( !$(this).parent().hasClass('dropdown')) {
+       $('.navbar-collapse').collapse('hide'); 
+    }
+ });
+
+
+const bgNav =  document.querySelector("nav")
+
+window.addEventListener("scroll", function(){
+  if (pageYOffset >= 200) {
+    bgNav.classList.remove("bg-transparent")
+    bgNav.classList.add("nav__sf-bg")
+  }
+  if (pageYOffset < 200){
+    bgNav.classList.add("bg-transparent")
+    bgNav.classList.remove("nav__sf-bg")
+  } 
+})
